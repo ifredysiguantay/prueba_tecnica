7c544aeae2efa86fe2f5be19a62e20bc1b8598ac
ifredyorlando <tooheyred@gmail.com>
2021-05-30 20:07:23 -0600

    cambio en la ruta principal de la pagina

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/','GeneralController@index');
 Route::resource('productos','GeneralController');
+
+
 Route::get('products_chart/{id}', 'ProductsChartController@index');
